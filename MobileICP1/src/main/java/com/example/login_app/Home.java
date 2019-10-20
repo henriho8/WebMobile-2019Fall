b10b93b82747a1c30f68958db311dd02ebf9a738
henriho8 <46879948+henriho8@users.noreply.github.com>
2019-10-19 23:27:58 -0500

    Add files via upload

---
+++ MobileICP1/src/main/java/com/example/login_app/Home.java
@@ -0,0 +1,40 @@
+package com.example.login_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import androidx.core.graphics.drawable.RoundedBitmapDrawable;
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
+
+public class Home extends AppCompatActivity {
+    //Logout button variable
+    private Button Logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Logout = (Button) findViewById(R.id.logout);
+
+        Bitmap batmapBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.hanzo);
+        RoundedBitmapDrawable circularBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), batmapBitmap);
+        circularBitmapDrawable.setCircular(true);
+
+        // Navigate from homepage to login page after logout button clicked
+        Logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Home.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
